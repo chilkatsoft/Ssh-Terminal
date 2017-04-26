14c97f36c90ece8e4d43522fbd6e9e447d537bdc
chilkatsoft <admin@chilkatsoft.com>
2017-04-26 16:45:30 -0500

    Embed SSH Terminal in C# Application

---
+++ Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Ssh_Terminal
+    {
+    partial class Form1
+        {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+            {
+            if (disposing && (components != null))
+                {
+                components.Dispose();
+                }
+            base.Dispose(disposing);
+            }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+            {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.txtDebug = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(717, 464);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 83);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Connect and Login (and start the terminal)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(166, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Host Name (or IP address)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(168, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(168, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password";
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(305, 17);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(253, 20);
+            this.txtHost.TabIndex = 5;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(305, 43);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(253, 20);
+            this.txtUsername.TabIndex = 6;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(305, 70);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(253, 20);
+            this.txtPassword.TabIndex = 7;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 106);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.textBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.txtDebug);
+            this.splitContainer1.Size = new System.Drawing.Size(1138, 464);
+            this.splitContainer1.SplitterDistance = 717;
+            this.splitContainer1.TabIndex = 8;
+            // 
+            // txtDebug
+            // 
+            this.txtDebug.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtDebug.Location = new System.Drawing.Point(0, 0);
+            this.txtDebug.Multiline = true;
+            this.txtDebug.Name = "txtDebug";
+            this.txtDebug.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtDebug.Size = new System.Drawing.Size(417, 464);
+            this.txtDebug.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1138, 570);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "SSH Terminal Sample (requires Chilkat v9.5.0.67 or later)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+            }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox txtDebug;
+        }
+    }
+
---
+++ Form1.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Ssh_Terminal
+    {
+    public partial class Form1 : Form
+        {
+        Chilkat.Ssh m_ssh = null;
+
+        private int SshChannel { get; set; }
+
+        // The current ongoing task will be either a read or send.
+        // Send tasks are only started when key presses are ready to send,
+        // thus a send task finishes quickly.  When it finishes, a new read
+        // task will be started unless there are more key presses to send.
+        // If a key press occurs, and a read task is ongoing, then it is cancelled
+        // an the send task is started.
+        // 
+        // Note: The m_currentTask is used in both the foreground and background threads (in TaskFinished),
+        // so always use Monitor.Enter(m_currentTask) and Monitor.Exit(m_currentTask)
+        // Never let m_currentTask be null.  Instead, check for a task.Status equal to "empty".
+        Chilkat.Task m_currentTask = new Chilkat.Task();
+
+        private Chilkat.StringBuilder m_accumulatedKeyPresses = new Chilkat.StringBuilder();
+
+        public Form1()
+            {
+            InitializeComponent();
+            }
+
+        private void Form1_Load(object sender, EventArgs e)
+            {
+            Chilkat.Global glob = new Chilkat.Global();
+            bool success = glob.UnlockBundle("Anything for 30-day trial");
+            if (!success)
+                {
+                MessageBox.Show("Chilkat trial expired.");
+                }
+
+            }
+
+        // Connect to an SSH server and login.
+        private void button1_Click(object sender, EventArgs e)
+            {
+            m_ssh = new Chilkat.Ssh();
+
+            bool success = m_ssh.Connect(txtHost.Text, 22);
+            if (!success)
+                {
+                textBox1.Text = m_ssh.LastErrorText;
+                return;
+                }
+
+            success = m_ssh.AuthenticatePw(txtUsername.Text, txtPassword.Text);
+            if (!success)
+                {
+                textBox1.Text = m_ssh.LastErrorText;
+                return;
+                }
+
+            // Use QuickShell instead of the chunk of commented-out code below..
+            SshChannel = m_ssh.QuickShell();
+            if (SshChannel < 0)
+                {
+                textBox1.Text = m_ssh.LastErrorText;
+                return;
+                }
+
+            //SshChannel = m_ssh.OpenSessionChannel();
+            //if (SshChannel < 0)
+            //    {
+            //    textBox1.Text = m_ssh.LastErrorText;
+            //    return;
+            //    }
+            //success = m_ssh.SendReqPty(SshChannel, "dumb", 80, 24, 0, 0);
+            //if (!success)
+            //    {
+            //    textBox1.Text = m_ssh.LastErrorText;
+            //    return;
+            //    }
+            //success = m_ssh.SendReqShell(SshChannel);
+            //if (!success)
+            //    {
+            //    textBox1.Text = m_ssh.LastErrorText;
+            //    return;
+            //    }
+
+
+            // Begin reading the channel  asynchronously and emit whatever arrives
+            // to textBox1.
+
+            m_ssh.OnTaskCompleted += m_ssh_OnTaskCompleted;
+
+            Monitor.Enter(m_currentTask);
+            Chilkat.Task t = m_currentTask;
+            m_currentTask = m_ssh.ChannelReadAsync(SshChannel);
+            if (m_currentTask != null)
+                {
+                m_currentTask.UserData = "read";
+                m_currentTask.Run();
+                }
+            else
+                {
+                // Don't let m_currentTask be null
+                m_currentTask = new Chilkat.Task();
+                }
+            Monitor.Exit(t);
+            }
+
+        void appendToTxtDebug(string s)
+            {
+            txtDebug.AppendText(s);
+            txtDebug.SelectionStart = txtDebug.Text.Length;
+            txtDebug.ScrollToCaret();
+            }
+
+        void bgAppendToTxtDebug(string s)
+            {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    txtDebug.AppendText(s);
+                    txtDebug.SelectionStart = txtDebug.Text.Length;
+                    txtDebug.ScrollToCaret();
+                });
+            }
+
+        void handleChannelReadCompleted(Chilkat.TaskCompletedEventArgs args)
+            {
+            int result = args.Task.GetResultInt();
+
+            string s = null;
+            bool bStartNextTask = false;
+
+            if (result == -1)
+                {
+                // Some error occurred..
+                s = m_ssh.LastErrorText;
+                }
+            else if (result == -2)
+                {
+                // No additional data arrived, just fall through to start a send or read.
+                bStartNextTask = true;
+                }
+            else if (result == 0)
+                {
+                // Check to see if the channel was closed or an EOF arrived..
+                if (m_ssh.ChannelReceivedClose(SshChannel))
+                    {
+                    s = "\r\n(SSH channel closed)\r\n";
+                    }
+                else if (m_ssh.ChannelReceivedEof(SshChannel))
+                    {
+                    s = "\r\n(Received EOF on SSH channel)\r\n";
+                    }
+                else
+                    {
+                    // Don't expect to get here.. just fall through and start another task.
+                    bStartNextTask = true;
+                    }
+                }
+            else if (result > 0)
+                {
+                // Data arrived..
+                s = m_ssh.GetReceivedText(SshChannel, "utf-8");
+                bStartNextTask = true;
+                }
+
+            // We're in a background thread.  We must update the textbox 
+            // in this way:
+            if (s != null)
+                {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    textBox1.AppendText(s); 
+                    textBox1.SelectionStart = textBox1.Text.Length;
+                    textBox1.ScrollToCaret();
+                });
+                }
+
+            Monitor.Enter(m_currentTask);
+            Chilkat.Task t = m_currentTask;
+            m_currentTask = new Chilkat.Task();
+            Monitor.Exit(t);
+
+            if (bStartNextTask)
+                {
+                startNextTask();
+                }
+            }
+
+        void startNextTask()
+            {
+            if (m_ssh == null) return;
+
+            // Start a send task if key presses are waiting to be sent,
+            // otherwise start another read task.
+            if (m_accumulatedKeyPresses.Length > 0)
+                {
+                string s = m_accumulatedKeyPresses.GetAsString();
+
+                // Log the string that we are sending as quoted-printable, so we can see the exact bytes.
+                bgAppendToTxtDebug("Sent " + m_accumulatedKeyPresses.GetEncoded("qp", "utf-8") + "\r\n");
+
+                m_accumulatedKeyPresses.Clear();
+
+                Monitor.Enter(m_currentTask);
+                Chilkat.Task t = m_currentTask;
+                m_currentTask = m_ssh.ChannelSendStringAsync(SshChannel, s, "utf-8");
+                if (m_currentTask != null)
+                    {
+                    m_currentTask.UserData = "send";
+                    m_currentTask.Run();
+                    }
+                else
+                    {
+                    // Don't let m_currentTask be null
+                    m_currentTask = new Chilkat.Task();
+                    }
+                Monitor.Exit(t);
+                }
+            else
+                {
+                Monitor.Enter(m_currentTask);
+                Chilkat.Task t = m_currentTask;
+                m_currentTask = m_ssh.ChannelReadAsync(SshChannel);
+                if (m_currentTask != null)
+                    {
+                    m_currentTask.UserData = "read";
+                    m_currentTask.Run();
+                    }
+                else
+                    {
+                    // Don't let m_currentTask be null
+                    m_currentTask = new Chilkat.Task();
+                    }
+                Monitor.Exit(t);
+                }
+            }
+
+        void handleChannelSendCompleted(Chilkat.TaskCompletedEventArgs args)
+            {
+            string s = null;
+            bool bStartNextTask = true;
+
+            // Get the return value of the ChannelSendString (which returns a boolean)
+            bool success = args.Task.GetResultBool();
+            if (!success)
+                {
+                // Failed to send the string..
+                s = m_ssh.LastErrorText;
+                bStartNextTask = false;
+                }
+
+            // We're in a background thread.  We must update the textbox 
+            // in this way:
+            if (s != null)
+                {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    textBox1.AppendText(s);
+                    textBox1.SelectionStart = textBox1.Text.Length;
+                    textBox1.ScrollToCaret();
+                });
+                }
+
+            Monitor.Enter(m_currentTask);
+            Chilkat.Task t = m_currentTask;
+            m_currentTask = new Chilkat.Task();
+            Monitor.Exit(t);
+            if (bStartNextTask)
+                {
+                startNextTask();
+                }
+
+            }
+
+        // Check to see what task completed and route to the correct handler..
+        void m_ssh_OnTaskCompleted(object sender, Chilkat.TaskCompletedEventArgs args)
+            {
+            // If this task was canceled, then just start the next task.
+            // This would typically happen when a key is pressed.  A read task will be ongoing, and the key press handler
+            // will cancel the read task so that a send task can be started.
+            if (args.Task.Status.Equals("canceled") || args.Task.Status.Equals("aborted"))
+                {
+                Monitor.Enter(m_currentTask);
+                Chilkat.Task t = m_currentTask;
+                m_currentTask = new Chilkat.Task();
+                Monitor.Exit(t);
+
+                startNextTask();
+                return;
+                }
+
+            if (args.Task.UserData.Equals("read"))
+                {
+                handleChannelReadCompleted(args);
+                }
+            else if (args.Task.UserData.Equals("send"))
+                {
+                handleChannelSendCompleted(args);
+                }
+            }
+
+        // This is called when a key is pressed in the textbox.
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+            {
+            m_accumulatedKeyPresses.Append(e.KeyChar.ToString());
+
+            Monitor.Enter(m_currentTask);
+
+            if (m_currentTask.Live)
+                {
+                // If the current task is a read task, then cancel it.
+                // The send task should automatically start because when the read task "finishes" because it was canceled,
+                // the task completed handler will see that accumulated key presses are waiting to be sent,
+                // and the send task will be started.
+                if (m_currentTask.UserData.Equals("read"))
+                    {
+                    //appendToTxtDebug("(keypress handler) Cancelling the current read task...\r\n");
+                    m_currentTask.Cancel();
+                    }
+                else
+                    {
+                    appendToTxtDebug("(keypress handler) Current task user data: " + m_currentTask.UserData + "\r\n");
+                    }
+                }
+
+            Monitor.Exit(m_currentTask);
+
+            e.Handled = true;
+
+            textBox1.SelectionStart = textBox1.Text.Length;
+            textBox1.ScrollToCaret();
+            //Application.DoEvents();
+            }
+
+
+        }
+    }
